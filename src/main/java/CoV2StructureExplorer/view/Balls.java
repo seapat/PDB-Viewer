22aba00fc588d06dbf2d94e763e4ac9ea84e3951
Sean <Sean@DESKTOP-9JUULL6>
2021-07-01 17:39:17 +0200

    Map instead of switch-case for colors

--- src/main/java/CoV2StructureExplorer/view/Balls.java
+++ src/main/java/CoV2StructureExplorer/view/Balls.java
@@ -7,56 +7,53 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 
+import java.util.AbstractMap;
+import java.util.Map;
+
 public class Balls extends Group {
 
     //sphere.setdrawMode(DrawMode.LINE) to see mesh used for drawing
 
+    private final static Map<Character, Color> colorMap = Map.ofEntries(
+            // single bonded radii found on wikipedia "covalent radius"
+            new AbstractMap.SimpleEntry<>('O', Color.RED),
+            new AbstractMap.SimpleEntry<>('C', Color.DIMGRAY),
+            new AbstractMap.SimpleEntry<>('N', Color.BLUE),
+            new AbstractMap.SimpleEntry<>('S', Color.YELLOW),
+            new AbstractMap.SimpleEntry<>('P', Color.ORANGE),
+            new AbstractMap.SimpleEntry<>('H', Color.WHITE)
+    );
+
     public Balls(Structure pdb, ReadOnlyDoubleProperty radiusScale, Integer modelChoice){
 
-        final int opacity = 1;
+//        final int opacity = 1;
 
         for (var chain: pdb.get(modelChoice -1 )){
             for (var residue: chain) {
                 for (var atom: residue) {
 
-                    final Sphere sphere = new Sphere(radiusScale.getValue(), 32); //32
-                    final Color color;
-
-                    // TODO: Maybe make this a separate method?
-                    switch (atom.getSimpleType()) {
-                        case 'O' -> {
-                            color = Color.RED.deriveColor(1,1,1, opacity);
-                        }
-                        case 'C' -> {
-                            color = Color.DARKGREY.deriveColor(1,1,1, opacity);
-                        }
-                        case 'N' -> {
-                            color = Color.BLUE.deriveColor(1,1,1, opacity);
-                        }
-                        case 'S' -> {
-                            color = Color.YELLOW.deriveColor(1,1,1, opacity);
-                        }
-                        case 'P' -> {
-                            color = Color.ORANGE.deriveColor(1,1,1, opacity);
-                        }
-                        case 'H' -> {
-                            color = Color.WHITE.deriveColor(1,1,1, opacity);
-                        }
-                        default -> {
-                            color = Color.PLUM.deriveColor(1,1,1, opacity);
-                        }
-                    }
+                    final Sphere sphere = new Sphere(100, 32);
+                    final Color color = colorMap.getOrDefault(atom.getSimpleType(), Color.PLUM);
 
                     sphere.setTranslateX(100 * atom.getPosition().x());
                     sphere.setTranslateY(100 * atom.getPosition().y());
                     sphere.setTranslateZ(100 * atom.getPosition().z());
-                    sphere.setMaterial(new PhongMaterial(color));
+
+                    PhongMaterial material = new PhongMaterial();
+                    material.setSpecularColor(Color.WHITE);
+                    material.setSpecularPower(20);
+                    material.setDiffuseColor(color);
+                    sphere.setMaterial(material);
+
                     sphere.radiusProperty().bind(radiusScale.multiply(atom.getRadius()));
                     getChildren().add(sphere);
                 }
             }
-
         }
+
+//        AmbientLight ambient = new AmbientLight();
+//        getChildren().add(ambient);
+        System.out.println("balls: " + this.getChildren().size());
 
     }
 
